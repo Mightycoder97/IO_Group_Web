7b96f59310991c3a831c036e9c82f49f58e9f5a8
Sebastian Retamozo <sebastian3197@gmail.com>
2026-02-03 10:10:37 -0500

    update

--- control/api/rutas.php
+++ control/api/rutas.php
@@ -103,9 +103,11 @@ function getOne($id) {
         
         // Get services for this route
         $servicios = db()->query(
-            "SELECT s.*, se.nombre_comercial as sede_nombre, se.distrito, se.direccion
+            "SELECT s.*, se.nombre_comercial as sede_nombre, se.distrito, se.direccion, se.tarifa_servicio,
+                    e.ruc as empresa_ruc
              FROM Servicio s 
              INNER JOIN Sede se ON s.id_sede = se.id_sede
+             INNER JOIN Empresa e ON se.id_empresa = e.id_empresa
              WHERE s.id_ruta = ? ORDER BY s.id_servicio",
             [$id]
         );
