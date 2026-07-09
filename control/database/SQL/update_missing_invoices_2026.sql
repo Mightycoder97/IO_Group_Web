057cceb65ef46f56b00c50971ab1c7f937e500da
Sebastian Retamozo <sebastian3197@gmail.com>
2026-07-09 10:29:37 -0500

    feat: generate migration script to recover missing invoices from SUNAT 2026 sales

---
+++ control/database/SQL/update_missing_invoices_2026.sql
@@ -0,0 +1,193 @@
+-- ========================================================
+-- SQL MIGRATION: UPDATE MISSING INVOICES FROM SUNAT 2026
+-- Generated on: 2026-07-09 10:29:29
+-- Total Invoices Recovered: 47
+-- ========================================================
+
+-- Recovered for client: VILCHEZ MARAVI ROSSE MERY (RUC: 10453900865)
+-- Service Date: 2026-03-31 | Amount: S/ 59.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (15767, '42348');
+
+-- Recovered for client: GAMARRA HEALTHCARE CENTER S.A.C. (RUC: 20600994302)
+-- Service Date: 2026-03-04 | Amount: S/ 1250.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (14742, '42389');
+
+-- Recovered for client: CLINICA VETERINARIA ALONZO S.A.C. (RUC: 20611374799)
+-- Service Date: 2026-03-31 | Amount: S/ 70.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (15759, '42444');
+
+-- Recovered for client: MUNICIPALIDAD DE JESUS MARIA (RUC: 20131366290)
+-- Service Date: 2026-03-23 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (15388, '42451');
+
+-- Recovered for client: LUMAR E&P S.A.C. (RUC: 20492602371)
+-- Service Date: 2026-03-10 | Amount: S/ 60.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (14929, '42572');
+
+-- Recovered for client: MUNICIPALIDAD DISTRITAL DE SURQUILLO (RUC: 20131372346)
+-- Service Date: 2026-03-13 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (15076, '42757');
+
+-- Recovered for client: CABALLERO NUÑEZ GUILLERMO ENRIQUE (RUC: 10033804364)
+-- Service Date: 2026-03-27 | Amount: S/ 55.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (15585, '42990');
+
+-- Recovered for client: SEGUNDA ETAPA AV. BRASIL N° 2722,2724,2724-A,2726,2728,2728-A,2730,2738,2740 AREA 1,379.26 M2. PUEBL (RUC: 20612164984)
+-- Service Date: 2026-04-04 | Amount: S/ 2500.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (15846, '43198');
+
+-- Recovered for client: AVALOS SANCHEZ PEDRO LUIS CARLOS (RUC: 10435868474)
+-- Service Date: 2026-04-25 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16642, '43201');
+
+-- Recovered for client: CALDERON MOYA CHRISTIAN VICTOR (RUC: 10700148527)
+-- Service Date: 2026-04-16 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16288, '43219');
+
+-- Recovered for client: DERMATOLOGICA PERU E.I.R.L. (RUC: 20601483701)
+-- Service Date: 2026-04-30 | Amount: S/ 1800.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16852, '43238');
+
+-- Recovered for client: PONCE URBINA CARLOS ALBERTO (RUC: 10704388913)
+-- Service Date: 2026-04-29 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16827, '43542');
+
+-- Recovered for client: NICASIO SILVA CESAR FORTUNATO (RUC: 10437153723)
+-- Service Date: 2026-04-30 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16843, '43543');
+
+-- Recovered for client: GALLO CABALLERO ANGELO JULIO GUILLERMO (RUC: 10099525661)
+-- Service Date: 2026-04-27 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16665, '43544');
+
+-- Recovered for client: MUNICIPALIDAD DISTRITAL DE SURQUILLO (RUC: 20131372346)
+-- Service Date: 2026-04-20 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16328, '43688');
+
+-- Recovered for client: GAMBINI CERCADO GINO ANTERO (RUC: 10722267791)
+-- Service Date: 2026-04-27 | Amount: S/ 55.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16697, '43748');
+
+-- Recovered for client: CABALLERO NUÑEZ GUILLERMO ENRIQUE (RUC: 10033804364)
+-- Service Date: 2026-04-28 | Amount: S/ 55.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16731, '43878');
+
+-- Recovered for client: BRIONES LAU LI MILAGROS (RUC: 10472000336)
+-- Service Date: 2026-04-23 | Amount: S/ 55.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16489, '43900');
+
+-- Recovered for client: REPRESENTACIONES MAGDA`S SOCIEDAD ANONIMA CERRADA (RUC: 20384463470)
+-- Service Date: 2026-04-13 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (16101, '43935');
+
+-- Recovered for client: CASTILLO ARCE JORMAN AARON (RUC: 10483985377)
+-- Service Date: 2026-01-30 | Amount: S/ 50.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13436, '40587');
+
+-- Recovered for client: CONSULTORIO DENTAL CORDOVA E.I.R.L. (RUC: 20609280329)
+-- Service Date: 2026-01-28 | Amount: S/ 60.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13328, '40603');
+
+-- Recovered for client: RRGASTROCENTER SOCIEDAD ANONIMA CERRADA (RUC: 20614114682)
+-- Service Date: 2026-01-31 | Amount: S/ 100.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13526, '40664');
+
+-- Recovered for client: SARMIENTO INFANTE JAIMAR JOHANA (RUC: 15606562613)
+-- Service Date: 2026-01-31 | Amount: S/ 200.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13484, '40671');
+
+-- Recovered for client: MEDAVAC-LAB S.A.C. (RUC: 20606809086)
+-- Service Date: 2026-01-15 | Amount: S/ 300.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (12842, '40701');
+
+-- Recovered for client: SALAZAR GUTIERREZ MIGUEL ANGEL (RUC: 10100259546)
+-- Service Date: 2026-01-26 | Amount: S/ 70.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13281, '40718');
+
+-- Recovered for client: CHAVEZ VILLANUEVA SUSAN SHIRLEY (RUC: 10425174733)
+-- Service Date: 2026-01-17 | Amount: S/ 55.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (12945, '40849');
+
+-- Recovered for client: NAVAS CORDERO MARIA PATRICIA (RUC: 15605059683)
+-- Service Date: 2026-01-29 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13366, '41013');
+
+-- Recovered for client: CENTRO MEDICO DE APOYO LOS ANGELES S.R.L. (RUC: 20607248380)
+-- Service Date: 2026-01-19 | Amount: S/ 60.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (12978, '41101');
+
+-- Recovered for client: MEDICVET INTEGRATIVA S.A.C. (RUC: 20612548430)
+-- Service Date: 2026-01-21 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13042, '41054');
+
+-- Recovered for client: MEDICVET INTEGRATIVA S.A.C. (RUC: 20612548430)
+-- Service Date: 2026-01-23 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13163, '41146');
+
+-- Recovered for client: TORRES AGUIRRE JORGE LUIS (RUC: 10225147081)
+-- Service Date: 2026-06-03 | Amount: S/ 59.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (18149, '44925');
+
+-- Recovered for client: MUNICIPALIDAD DE JESUS MARIA (RUC: 20131366290)
+-- Service Date: 2026-06-18 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (18667, '44920');
+
+-- Recovered for client: ASOCIACION CIVIL SAN MARTIN DE PORRES PERU ONG (RUC: 20609198801)
+-- Service Date: 2026-06-25 | Amount: S/ 350.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (19010, '44966');
+
+-- Recovered for client: SEGUNDA ETAPA AV. BRASIL N° 2722,2724,2724-A,2726,2728,2728-A,2730,2738,2740 AREA 1,379.26 M2. PUEBL (RUC: 20612164984)
+-- Service Date: 2026-06-04 | Amount: S/ 2500.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (18176, '45059');
+
+-- Recovered for client: SERVICIOS VETERINARIOS SEBASMEDIC E.I.R.L. (RUC: 20613400002)
+-- Service Date: 2026-06-18 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (18691, '45283');
+
+-- Recovered for client: CLINICA VETERINARIA DEL NORTE S.A.C. (RUC: 20613146149)
+-- Service Date: 2026-06-25 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (18960, '45478');
+
+-- Recovered for client: Carmela Maura Juarez Alvarez (RUC: 10072540463)
+-- Service Date: 2026-06-27 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (19092, '45499');
+
+-- Recovered for client: Daysi Yvett Diaz Sanchez (RUC: 10478360041)
+-- Service Date: 2026-05-30 | Amount: S/ 70.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (18011, '44090');
+
+-- Recovered for client: FREDDY LEON VENTOCILLA (RUC: 10103587692)
+-- Service Date: 2026-05-04 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (17002, '44199');
+
+-- Recovered for client: KING VET S.A.C. (RUC: 20609560844)
+-- Service Date: 2026-05-12 | Amount: S/ 60.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (17281, '44369');
+
+-- Recovered for client: MUNICIPALIDAD DISTRITAL DE SURQUILLO (RUC: 20131372346)
+-- Service Date: 2026-05-19 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (17537, '44557');
+
+-- Recovered for client: SEGUNDA ETAPA AV. BRASIL N° 2722,2724,2724-A,2726,2728,2728-A,2730,2738,2740 AREA 1,379.26 M2. PUEBL (RUC: 20612164984)
+-- Service Date: 2026-05-07 | Amount: S/ 2500.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (17150, '44547');
+
+-- Recovered for client: MUNICIPALIDAD DISTRITAL DE SURQUILLO (RUC: 20131372346)
+-- Service Date: 2026-02-13 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13987, '41550');
+
+-- Recovered for client: DR. FRANCISCO ROMAN M. E.I.R.L. (RUC: 20604590371)
+-- Service Date: 2026-02-28 | Amount: S/ 120.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (14631, '41615');
+
+-- Recovered for client: MUNICIPALIDAD DE JESUS MARIA (RUC: 20131366290)
+-- Service Date: 2026-02-19 | Amount: S/ 65.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (14202, '41760');
+
+-- Recovered for client: WALDEMAR DAVILA MELGAREJO (RUC: 10060022963)
+-- Service Date: 2026-02-10 | Amount: S/ 55.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (13826, '41817');
+
+-- Recovered for client: OFTALMO MEDICAL CENTER S.A.C. (RUC: 20607132454)
+-- Service Date: 2026-02-20 | Amount: S/ 90.00
+INSERT INTO `Factura` (`id_servicio`, `numero_factura`) VALUES (14281, '42032');
